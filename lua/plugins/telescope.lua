73342b19c5cb11869c5befff3edeb09b8290fd8d
jimrothstein <jimrothstein@gmail.com>
2023-05-21 20:04:38 -0700

    add plugins/telescope.lua

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- disable the keymap to grep files
+    -- {"<leader>/", false},
+    -- change a keymap
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files in cwd" },
+
+    -- jr added:
+    {
+      "<leader>fz",
+      function()
+        require("telescope.builtin").find_files({ cwd = "~" })
+      end,
+      desc = "HOME ...",
+    },
+
+    -- add a keymap to browse plugin files
+    --
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
+    },
+  },
+}
