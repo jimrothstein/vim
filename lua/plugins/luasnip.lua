c596e1aa2d4619610a1f56c3080f424fb73246e3
jimrothstein <jimrothstein@gmail.com>
2023-06-04 14:07:12 -0700

    restore plugins/luasnip.lua to 11 days ago

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,13 +1,11 @@
 return {
-  -- 2023-06-02
-  -- attempt to restore to ORIGINAL config (that once worked)
   "L3MON4D3/LuaSnip",
   -- "rafamadriz/friendly-snippets",
   config = function()
-    local path = vim.fn.stdpath("config") .. "/snippets.lua"
-    --
+    local path = vim.fn.stdpath("config") .. "/snippets"
+
     require("luasnip.loaders.from_lua").lazy_load({
       paths = path,
     })
-  end, -- end functin
+  end,
 }
