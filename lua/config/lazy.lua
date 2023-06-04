43b4b8d5daa8dcaa956215fe17ec5decafe01c51
jimrothstein <jimrothstein@gmail.com>
2023-06-04 08:44:42 -0700

    wip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- By default, only LazyVim plugins will be lazy-loaded. (jr:) YOUR CUSTOM PLUGINS WILL LOAD DURING STARTUP.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
