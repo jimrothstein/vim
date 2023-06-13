adb5365e03ec2170214f629aad3fd33819ccc4d9
jimrothstein <jimrothstein@gmail.com>
2023-06-13 15:09:38 -0700

    13JUNE - This is last commit of working (sort of) lazyvim starter

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 -- Want plain vanilla nvim?    Comment this line.
 require("config.lazy")
+--
+--
+-- Check is this loading?  (yes = light screen)
+-- vim.cmd.colorscheme('morning')  -- light
+-- vim.cmd.colorscheme('habamax')  -- dark
